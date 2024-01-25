1f15d8fd6c3d66a419362fb428f6db0f65e13760
Marc Lyster Canillo <156999510+MarcHAHAHA@users.noreply.github.com>
2024-01-25 16:28:04 +0800

    Add files via upload

---
+++ CANILLO_ACTIVITY_II.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+  {
+    int num1;
+    int num2;
+
+    cout << "enter first number: ";
+    cin >> num1;
+    cout << "enter second number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    int diff = num1 - num2;
+    int prod = num1 * num2;
+
+    cout << "sum = "<< num1 + num2;
+    cout << "diff = "<< num1 - num2;
+    cout << "prod = "<< num1 * num2;
+
+
+  }
