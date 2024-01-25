454edee3191e12178fe3e8e7414279a2a7d08e19
Sheenley Von P. Ypon <156996942+Sheenley@users.noreply.github.com>
2024-01-25 17:15:12 +0800

    Add files via upload
    I LOVE YOU SIR!

---
+++ Activities/Activity II/YPON_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class YPON_ACTIVITY_II{
+
+    public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter frist number: ");
+            int fnum = input.nextInt();
+            System.out.println("Enter second number: ");
+            int snum = input.nextInt();
+
+            int sum = fnum + snum;
+            int diff = fnum - snum;
+            int quo = fnum / snum;
+            int pro = fnum * snum;
+
+            System.out.println(" The snum is " + sum);
+            System.out.println(" The difference is " + diff);
+            System.out.println(" The quotient is " + quo);
+            System.out.println(" The product is " + pro);
+
+    }
+}
