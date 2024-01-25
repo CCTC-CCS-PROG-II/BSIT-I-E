1c3dee735b360b73189b7c4f249c5a24d1feabeb
Medallo S. Reynaldo Jr <156997504+medallo272004@users.noreply.github.com>
2024-01-25 17:16:41 +0800

    Add files via upload

---
+++ Activities/Activity II/MEDALLO_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System,In);
+        int num;
+        int num;
+        
+        System.out.println("Enter two numbers: ");
+        int snum = sc.nextInt ();
+        
+        int sum = fnum + snum;
+        int diff = fnum + snum;
+        int prod = fnum + snum;
+        
+        System.out.println("Sum = "+ sum)
+        System,out.println("Difference = "+ diff)
+        System.out.println("Product = "+prod)
+        
+
+
+    }
+}
