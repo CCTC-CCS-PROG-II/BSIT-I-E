5fb6e9c8b7bbe7d09719cee34136ada75bfa1908
AGUANTA LEE <157687146+Aguanta1@users.noreply.github.com>
2024-01-25 17:23:33 +0800

    Add files via upload

---
+++ Activities/Activity II/ACTIVITY_prog2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+    public class ACTIVITY_prog2 {
+
+       public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+       System.out.print("Enter first number: ");
+       int fnum = input.nextInt();
+       System.out.print("Enter second number: ");
+       int snum = input.nextInt();
+       int sum = fnum + snum;
+       System.out.println("The sum is " + sum);
+       System.out.println("The sum is " + sum);
+   }
+}
