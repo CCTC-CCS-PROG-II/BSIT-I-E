24cf96c870766646ea30b211771d2d118f6a85f5
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-01-25 17:15:33 +0800

    Add files via upload

---
+++ Activities/Activity II/CANTEBEROS_ACTIVITY_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class solve{
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+
+        int num1
+        int num2
+
+        System.out.print("Enter first number: ");
+        int num = scanner.nextLine();
+        System.out.println("Enter second number: ");
+        int num = scanner.nextLine();
+        int num = num1 + num2;
+
+    }
+}
