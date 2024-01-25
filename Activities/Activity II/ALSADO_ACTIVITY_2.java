9a3f724f579f83c6ac70c84b6e133e04c6ebcb87
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-01-25 17:18:28 +0800

    Add files via upload

---
+++ Activities/Activity II/ALSADO_ACTIVITY_2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ALSADO_ACTIVITY_2{
+    public static void main(String[] args){
+
+        Scanner Burikatsiya = new Scanner(System.in);
+            int num1;
+            int num2;
+            
+            System.out.print("Enter the first number: ");
+            num1 = Burikatsiya.nextInt();
+
+            System.out.print("Enter the second number: ");
+            num2 = Burikatsiya.nextInt();
+
+            int Sum = num1 + num2;
+            int Difference = num1 - num2;
+            int Product = num1 * num2;
+
+            System.out.println("Sum = " + Sum);
+            System.out.println("Sub = " + Difference);
+            System.out.println("Mul =" + Product);
+    
+    }
+}
