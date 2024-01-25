588ddc9c239fd96d7973517cbe9c5a50130298b5
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-01-25 17:19:06 +0800

    Add files via upload

---
+++ Activities/Activity II/ABATAYO_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ABATAYO_ACTIVITY_II{
+
+
+    public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    int num1;
+    int num2;
+
+
+        System.out.printIn("Enter your name:");
+    int num = scanner. nextline();
+        System .out.printIn("Enter your password:");
+    int num = scanner.nextline();
+    int num = num1 + num2;
+
+    
+        System,out.printIn(x:"Info");
+    }
+
+}
