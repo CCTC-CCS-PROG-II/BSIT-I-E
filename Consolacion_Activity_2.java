79b3b42760202928e541887216c7999a30996810
Hazzel Consolacion <156997534+hazehazyyyy@users.noreply.github.com>
2024-01-25 17:21:57 +0800

    Add files via upload

---
+++ Consolacion_Activity_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Consolacion_Activity_2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int fnum = input.nextInt();
+        System.out.print("Enter second number: ");
+        int snum = input.nextInt();
+        int sum = fnum + snum;
+        System.out.println("The sum is " + sum);
+    }
+}
