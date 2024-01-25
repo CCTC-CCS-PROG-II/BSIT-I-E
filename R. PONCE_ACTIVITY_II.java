d59ede02b2aa93147fab0efb23c5ff36917b3ade
Ricky Ponce <156997773+rickyboyyyyy@users.noreply.github.com>
2024-01-25 17:15:12 +0800

    Add files via upload

---
+++ R. PONCE_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner
+public class R.PONCE
+
+public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+
+    int num1
+    int num2
+
+    System.out.println(x"Enter your First Number: ");
+    int num = scanner.nextln();
+    System.out.println(x"Enter your password");
+    int num = scanner.nextline();
+    int num = num1 + num2;
+
+
+    System.out.println(x"Info");
+
+
+}   
