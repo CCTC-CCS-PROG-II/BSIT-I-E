002b04a742034146083c5a63b0575cb4f3c45739
angielicahermosa <157686368+angielicahermosa@users.noreply.github.com>
2024-01-25 17:22:38 +0800

    Add files via upload

---
+++ Activities/Activity II/Hermosa_Activity_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hermosa_Activity_2 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number");
+        int fnum = input.nextInt();
+        System.out.print("Enter second number");
+        int snum = input.nextInt();
+        int sum = fnum + snum;
+        System.out.println("The sum is " + sum);
+    }
+}
