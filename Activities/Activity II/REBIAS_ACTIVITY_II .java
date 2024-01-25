48bf08d76e4ff96253a791ef77974bd9ec4f67bf
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-01-25 17:16:25 +0800

    Add files via upload

---
+++ Activities/Activity II/REBIAS_ACTIVITY_II .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class numbers{
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        
+        int num1
+        int num2
+
+            Sytstem.out.println("Enter first number");
+       int num = scanner.nextline();
+            System.out.println("Enter seccond number");
+       int num = scanner.nextlne();
+       int num = num1 + num2;
+            
+}
