b4b0765c8486ccf25eb343ccc34a083e1644027e
Cris Jeano Dejado <156996907+jeanogwapo@users.noreply.github.com>
2024-02-01 16:41:43 +0800

    DEJADO_ACTIVITY_IV.java

---
+++ Activities/Activity IV/DEJADO_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class DEJADO_ACTIVITY.java{
+
+public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+System.out.print(s:"ENTER COLOR");
+String robot = sc.nextline();
+
+switch(robot){
+    case "RED"
+    System.out.print(s:"NUMBER 1");
+    break;
+    case "GREEN"
+    System.out.print(s:"NUMBER 2");
+    break;
+    case "BLACK"
+    System.out.print(s:"NUMBER 3");
+    break;
+    default;
+    System.out.print(s:"none");
+}
+}
+
+}
+
