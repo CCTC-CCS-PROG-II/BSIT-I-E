e361d0eca0161527b16495b11813f1a47dceaab8
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-02-01 16:48:49 +0800

    Update LIBRADO_ACTIVITY_4.java
    final nani

--- Activities/Activity IV/LIBRADO_ACTIVITY_4.java
+++ Activities/Activity IV/LIBRADO_ACTIVITY_4.java
@@ -4,10 +4,7 @@ public class LIBRADO_ACTIVITY_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a color: ");
-        System.out.println("Red Goes to box 1");
-        System.out.println("Green Goes to box 2");
-        System.out.println("Black Goes to box 3");
+        System.out.print("Enter a color base on the following choices red goes to box 1, green goes to box 2, black goes to box 3 (red, green, black): ");
         String color = scanner.nextLine();
 
         switch (color) {
