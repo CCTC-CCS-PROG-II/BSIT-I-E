9e9455598191d608f770789f5a4052caf4747a85
Doven <156999719+Ventaejin@users.noreply.github.com>
2024-02-01 16:42:10 +0800

    Add files via upload

---
+++ Activities/Activity IV/REYES_ACTIVITY_4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class REYES_ACTIVITY_4 {
+   
+    public static void main (String []args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print( "Enter a Color:");
+        String robot = sc.nextLine();
+            
+            switch(robot){
+                case "red":
+                System.out.print("box #1");
+                break;
+                case "green":
+                System.out.print("box #2");
+                break;
+                case "black":
+                System.out.print("box #3");
+                break;
+                default:
+                System.out.print("Error");
+
+        }
+    }
+}
