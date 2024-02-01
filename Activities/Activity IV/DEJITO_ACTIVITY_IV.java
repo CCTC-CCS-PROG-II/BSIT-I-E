7c6aee815dfe589caecb2269e6f1fc92d11512e0
Shanelle Dejito <156997093+shanellegwapa123@users.noreply.github.com>
2024-02-01 16:37:31 +0800

    DEJITO_ACTIVITY_IV.java

---
+++ Activities/Activity IV/DEJITO_ACTIVITY_IV.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DEJITO_ACTIVITY_IV {
+   
+    public static void main (String []args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print( "ENTER COLOR");
+        String robot = sc.nextLine();
+            
+            switch(robot){
+                case "RED":
+                System.out.print("NUMBER 1");
+                break;
+                case "GREEN":
+                System.out.print("NUMBER 2");
+                break;
+                case "BLACK":
+                System.out.print("NUMBER 3");
+                break;
+                default:
+                System.out.print("wala");
+
+        }
+    }
+}
+  
