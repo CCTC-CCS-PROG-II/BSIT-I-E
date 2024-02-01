c45a8346eef9a81b68adbc210e33338a6da37052
Francis John Panuncillo <156996985+pransthegreat@users.noreply.github.com>
2024-02-01 16:40:35 +0800

    PANUNCILLO_ACTIVITY_IV.java

---
+++ Activities/Activity IV/PANUNCILLO_ACTIVITY_IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PANUNCILLO_ACTIVITY_IV{
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+    System.out.print("ENTER COLOR");
+    String robot = sc.nextline();
+    
+    switch(robot){
+      case "RED":
+      System.out.print("NUMBER 1");
+      break;
+      case "GREEN":
+      System.out.print("NUMBER 2");
+      break;
+      case "BLACK":
+      System.out.print("NUMBER 3");
+      break;
+      default:
+      System.out.print("none");
+    }
+    }
+}
+
+  
