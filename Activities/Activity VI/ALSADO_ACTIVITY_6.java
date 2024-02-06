8be08f9b936ebf581dacdb556614a3ae7ce76c67
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-02-06 17:31:10 +0800

    Add files via upload

---
+++ Activities/Activity VI/ALSADO_ACTIVITY_6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ALSADO_ACTIVITY_6 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int num = sc.nextInt();
+        
+        int sum = 0;
+        int i = 1;
+        while(i <= num){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
