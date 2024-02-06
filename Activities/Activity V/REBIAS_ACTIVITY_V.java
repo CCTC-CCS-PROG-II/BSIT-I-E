8ee01515975036f3cefddea6e8b2aa39a44c269f
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-02-06 16:51:45 +0800

    Add files via upload

--- Activities/Activity V/REBIAS_ACTIVITY_V.java
+++ Activities/Activity V/REBIAS_ACTIVITY_V.java
@@ -1,23 +1,19 @@
-import java.uti.Scanner;
+import java.util.Scanner;
 public class REBIAS_ACTIVITY_V {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int age;
 
-        do{
             System.out.print("Enter age: ");
             age = input.nextInt();
-            if(age>= 0 && age <= 11){
-                System.out.print("Child");    
-            }
-            else if(age >=12 && age <= 17){
+            if(age>= 0 && age <= 11) {
+                System.out.print("Child");
+
+            }else if(age >=12 && age <= 17) {
                 System.out.println("Teen");
-            }
-            else if(age >=18 && age <=64){
+
+            }else if(age >=18 && age <=64) {
                 System.out.println("Adult");
-            }
-             
         }
     }
 }
