45c263841194c8b740de210276fe18979220b2d6
Rieda Mae Ygay <156997539+RiedaYgay@users.noreply.github.com>
2024-02-06 17:34:47 +0800

    Add files via upload

---
+++ Activities/Activity VI/YGAY_ACTIVITY_VI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class YGAY_ACTIVITY_VI {
+
+    public static void main(String []args){
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.prinIn(x:"Enter a number");
+        int num = sc nextInt();
+
+        int dum = 0;
+        
+        int i = 1;
+
+        while(i <=num){
+            sum += i;
+            i++;
+
+
+
+        }
+        System.out,println(sum);
+    } 
+
+}
+
