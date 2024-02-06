319a9aec071ce3665ed5c8d2fc7fe5fc8b06efb0
Sheenley Von P. Ypon <156996942+Sheenley@users.noreply.github.com>
2024-02-06 17:28:45 +0800

    Add files via upload

---
+++ Activities/Activity VI/YPON_ACTIVITY_VI.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class YPON_ACTIVITY_VI {
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your numebr: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        int i =1;
+
+        while(i <= num){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+
+    }
+    
+}
