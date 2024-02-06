ba642677f934acf1709b98457dd152657694f28f
Cris Jeano Dejado <156996907+jeanogwapo@users.noreply.github.com>
2024-02-06 16:15:52 +0800

    DEJADP_ACTIVITY_5.java

---
+++ DEJADP_ACTIVITY_5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class DEJADP_ACTIVITY_5 {
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in)
+        System.out.print("Enter ur age");
+        int age = sc.nextInt();
+        
+        if(age < 64 && age >18){
+            System.out.println("Adult");
+        }else if(age<=17 && age>=12 );
+    }else{
+        System.out.println("Teen")
+    }else{
+        System.out.println("Child");
+    
+
+        }
+    }
+}
+
