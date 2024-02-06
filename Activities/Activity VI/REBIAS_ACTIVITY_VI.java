d7e9b112ecfd061d3b3fea7f4c2421423e990559
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-02-06 17:34:52 +0800

    Add files via upload

---
+++ Activities/Activity VI/REBIAS_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import.java.util.Scanner;
+public class REBIAS_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int num = sc.nextln();
+
+        int sum = 0;
+
+        int i = 1;
+
+        while(i <= num){
+            sum +=
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
+
+
