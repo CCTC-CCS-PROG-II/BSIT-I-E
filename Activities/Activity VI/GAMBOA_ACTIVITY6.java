5a26d13b54b546fce5131ce822254e01ba07dd90
Reymark T Prahinog <156998614+KramyerOret@users.noreply.github.com>
2024-02-06 17:36:36 +0800

    Add files via upload

---
+++ Activities/Activity VI/GAMBOA_ACTIVITY6.java
@@ -0,0 +1,19 @@
+public class GAMBOA_ACTIVITY6 {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+         int num = sc.nextInt();
+        
+         int sum = 0;
+         
+         int i = 1;
+         
+         while(num > 0){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
+
