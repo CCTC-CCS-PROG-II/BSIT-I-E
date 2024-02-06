41742d45dec56b58ce2764933e8edece57768679
Hazzel Consolacion <156997534+hazehazyyyy@users.noreply.github.com>
2024-02-06 17:33:48 +0800

    Add files via upload

---
+++ Activities/Activity VI/CONSOLACION_ACTIVITY_6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CONSOLACION_ACTIVITY_6 {
+
+    public class InnerCONSOLACION_ACTIVITY_6 {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+
+        int sum = 0
+
+        int i = 1;
+
+        while(i << num){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);1
+    }
+}
