bd0252cc0ee2459f8f1babe67cbd74adab61fc11
Cris Jeano Dejado <156996907+jeanogwapo@users.noreply.github.com>
2024-02-06 17:30:13 +0800

    DEJADO_ACTIVITY_6.java

---
+++ Activities/Activity VI/DEJADO_ACTIVITY_6.java
@@ -0,0 +1,17 @@
+port java.util.Scanner;
+public class DEJADO_ACTIVITY_lV {
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number;");
+        int num = sc.nextInt();
+
+        int sum = 0;
+         int 1 = 1;
+         while(i <= num){
+            sum +=;
+         }
+         System.out.println(sum);
+         }
+    
+}
