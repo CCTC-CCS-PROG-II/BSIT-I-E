14e273786b004b9e36c5f2ca03a5aa6eb8eadb11
angielicahermosa <157686368+angielicahermosa@users.noreply.github.com>
2024-02-06 17:33:55 +0800

    Add files via upload

---
+++ Activities/Activity VI/HERMOSA_ACTIVITY6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HERMOSA_ACTIVITY6 {
+    
+    public class InnerHERMOSA_ACTIVITY6{
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int sum = 0
+
+        int i = 1;
+
+        while(i << num){
+            sum += 1;
+            1++;
+        }
+        System.out.println(sum);
+    }
+}
