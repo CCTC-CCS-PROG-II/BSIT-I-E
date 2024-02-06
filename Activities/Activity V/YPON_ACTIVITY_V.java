824bce7a330d8aa8f766cfa2b3c643e274299b26
Sheenley Von P. Ypon <156996942+Sheenley@users.noreply.github.com>
2024-02-06 16:09:02 +0800

    Add files via upload

---
+++ Activities/Activity V/YPON_ACTIVITY_V.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class YPON_ACTIVITY_V {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+        System.out.println("Enter your age: ");
+        age = sc.nextInt();
+
+        if (age >= 0 && age <= 11){
+            System.out.println("You are a Child");
+        }
+        else if (age >= 12 && age <= 17){
+            System.out.println("You are a Teenager");
+        }
+        else if (age >= 18 && age <= 64){
+            System.out.println("You are an Adult");
+        }
+
+    }
+
+}
