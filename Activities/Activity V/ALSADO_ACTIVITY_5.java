f89a299d87437894285f67b88c387fcdfde543d8
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-02-06 16:18:08 +0800

    Add files via upload

---
+++ Activities/Activity V/ALSADO_ACTIVITY_5.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+
+public class ALSADO_ACTIVITY_5 {
+    
+    public static void main(String[] args) {
+        
+        Scanner Ages = new Scanner(System.in);
+        System.err.print("Enter the age of the person:");
+        int age = Ages.nextInt();
+
+        if (age >= 0 && age <= 11){
+            System.out.println("Child");
+        } else if (age >= 12 && age <= 17) {
+            System.out.println("Teen");
+        } else if (age >= 18 && age <= 64) {
+            System.out.println("Adult");
+        } else {
+            System.out.println(" Invalid age. Please enter a non-negative age");
+        }
+    }
+}
