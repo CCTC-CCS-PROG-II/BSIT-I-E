1b8f8e3255de2d3a7bd383033430ba85b5ed3b08
Jay P. Plarisan <156997011+BSITjay@users.noreply.github.com>
2024-02-08 16:07:50 +0800

    Add files via upload
    rapxz

---
+++ Activities/Activity VI/PLARISAN_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PLARISAN_ACTIVITY_VI {
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        int time = 0;
+        System.out.println("Enter your time: ");
+         time = sc.nextInt();
+
+
+         while(time >= 0){
+            time--;
+           
+         }
+         System.out.println(time);
+
+    }
+    
+}
