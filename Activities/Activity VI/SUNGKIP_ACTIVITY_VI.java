ae1218f68b8831f95d1501d235b1461a18dadf32
andrewsungkip <156997562+andrewsungkip@users.noreply.github.com>
2024-02-08 16:09:29 +0800

    Add files via upload

---
+++ Activities/Activity VI/SUNGKIP_ACTIVITY_VI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SUNGKIP_ACTIVITY_VI {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the num : ");
+         num = sc.nextInt(); 
+          int num = 0;
+        while(num >= 0) {
+            num = num-1;
+       
+        }
+    }
+}
