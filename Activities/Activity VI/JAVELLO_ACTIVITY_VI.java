5592b50b0aff6189ff806fd9471779a6be51df13
Jake Christian Javello <156996977+jakeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-02-08 16:09:06 +0800

    Add files via upload

---
+++ Activities/Activity VI/JAVELLO_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class JAVELLO_ACTIVITY_VI{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of seconds:");
+        int seconds = scanner.nextInt();
+
+        for (int i = seconds; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Countdown finished!");
+    }
+}
