8a38910ff2022b2cf25016093af7f3c44bf3309f
Hazzel Consolacion <156997534+hazehazyyyy@users.noreply.github.com>
2024-02-08 16:10:28 +0800

    Add files via upload

---
+++ Activities/Activity VI/CONSOLACION_ACTIVITY_6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CONSOLACION_ACTIVITY_6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of seconds:");
+        int seconds = scanner.nextInt();
+
+        for (int i = seconds; i > 0; i--){
+            System.out.println(i);
+        }
+
+        System.out.println("Countdown finished!");
+    }
+}
