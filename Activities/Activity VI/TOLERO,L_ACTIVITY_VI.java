25bd3566cfdc3bb7e107819b07f2afcf9b584b37
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-02-08 16:08:36 +0800

    Add files via upload

---
+++ Activities/Activity VI/TOLERO,L_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PANUNCILLO_ACTIVITY_IV{
+    public static void main (String []args);
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("ENTER THE NUMBER OF SECONDS: ");
+    int seconds = scanner.nextInt();
+
+    for (int i = seconds; i > 0; i--) {
+        system.out.println(i);
+    }
+
+    System.out.println("Countdown finished!";)
+  }
+}
