01abea5ecfa3da31ad48c760387eb75bb6cbde1b
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-02-08 16:10:20 +0800

    Update TOLERO,L_ACTIVITY_VI.java

--- Activities/Activity VI/TOLERO,L_ACTIVITY_VI.java
+++ Activities/Activity VI/TOLERO,L_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TOLERO_ACTIVITY_IV{
+public class TOLERO,L_ACTIVITY_IV{
     public static void main (String []args);
 
     Scanner sc = new Scanner(System.in);
