13f69b6d8b73602e84354b6d3a212b7b9e49cb0f
Sheenley Von P. Ypon <156996942+Sheenley@users.noreply.github.com>
2024-02-08 16:08:13 +0800

    Add files via upload

---
+++ Activities/Activity VI/YPON_ACTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class YPON_ACTIVITY_VI {
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+
+        System.out.println("Enter the time (in seconds): ");
+         int time = sc.nextInt();
+
+
+
+        while(time >= 0){
+            time --;
+        }
+        System.out.println(time);
+
+    }
+    
+}
