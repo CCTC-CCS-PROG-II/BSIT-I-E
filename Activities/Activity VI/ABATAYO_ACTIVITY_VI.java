7fdad3ef4abd2cfc5dbb4814552ddd9fa72fda87
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-02-08 16:08:15 +0800

    Add files via upload

---
+++ Activities/Activity VI/ABATAYO_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ABATAYO_ACTIVITY_VI { 
+    public static void main(String []args) {
+       Scanner sc = Scanner(System.in);
+       
+       System.out.print("Enter a countdown number: ");
+       int counter = sc.nextInt();
+       int counter 2 = sc.nextlnt();
+       while(counter <= 0)
+           counter--:
+           System.out.println(counter);
+       }
+
+    }
+   
+} 
+
+  
