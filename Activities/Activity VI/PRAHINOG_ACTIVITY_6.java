10580abe2db0f34de0f50573538be9869150c99f
Reymark T Prahinog <156998614+KramyerOret@users.noreply.github.com>
2024-02-08 16:11:24 +0800

    Add files via upload

---
+++ Activities/Activity VI/PRAHINOG_ACTIVITY_6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PRAHINOG_ACTIVITY6 {
+    public static void main(String[] args) {
+        int seconds = 5; // Countdown from 5 seconds
+        
+        System.out.println("Counting down...");
+        
+        for (int i = seconds; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000); // Sleep for 1 second (1000 milliseconds)
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        System.out.println("Time's up!");
+    }
+}
