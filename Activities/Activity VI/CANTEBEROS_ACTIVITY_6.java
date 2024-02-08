a91272136a3186794f27eae9066cc30fb6906e65
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-02-08 16:08:04 +0800

    Add files via upload

---
+++ Activities/Activity VI/CANTEBEROS_ACTIVITY_6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CANTEBEROS_ACTIVITY_6 {
+
+    public static void main(String[] ) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter time (in seconds): ");
+        int time = sc nextInt();
+
+        while (time >= 0) {
+            Systen.out.print
+        }
+    }
+}
