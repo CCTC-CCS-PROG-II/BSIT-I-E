6e51883cd53c8f877aa1586b9e22d7e971507bd8
Chito Lacanglacang <156996675+Cheeeeeeeeeeeto@users.noreply.github.com>
2024-02-08 16:10:00 +0800

    Add files via upload

---
+++ Activities/Activity VI/LACANGLACANG_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LACANGLACANG_ACTIVITY_VI{
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+      System.out.println("Enter countdown number: ");
+      int counter = sc.nextInt();
+    
+
+      while(counter>=0){
+        System.out.println(counter);
+      }
+
+
+    }
+}
