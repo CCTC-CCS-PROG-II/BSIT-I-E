b86408c7c197984ff9ff2c769d11c294c3a86951
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-02-08 16:07:55 +0800

    Add files via upload

---
+++ Activities/Activity VI/LIBRADO_ACTIVITY_6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LIBRADO_ACTIVITY_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Enter the time in seconds: ");
+     int time = scanner.nextInt();
+     while (time >=0){
+        System.out.println(time);
+     }
+
+     
+    }
+}
+
