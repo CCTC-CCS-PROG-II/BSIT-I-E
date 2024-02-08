a3e7007f1859331cb29c3d57a470a31489bc721c
Shanelle Dejito <156997093+shanellegwapa123@users.noreply.github.com>
2024-02-08 16:11:21 +0800

    Add files via upload

---
+++ Activities/Activity VI/DEJITO_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DEJITO_ACTIVITY_VI{
+    public static void main (String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "Enter the number in seconds");
+        int seconds = sc.nextInt();
+        
+       for (int i = seconds; 
+        i > 0;
+        i--){
+         System.out.println (i);   
+        }
+        System.out.println ("Countdown finished!");
+
+}
+
+}
