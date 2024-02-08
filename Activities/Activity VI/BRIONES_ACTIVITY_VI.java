efdbeeeadbcce126ff6ac5c35d0f97c993e28a3b
Ganny Briones <156997601+gannybear1@users.noreply.github.com>
2024-02-08 16:09:23 +0800

    BRIONES_ACTIVITY_VI.java

---
+++ Activities/Activity VI/BRIONES_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BRIONES_ACTIVITY_VI{
+    public static void main(String []args){
+         Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter countdown number: ");
+    int counter = scanner.nextInt();
+    
+    while(counter <= 0){
+    counter -=;  
+    counter --;
+        System.out.println(counter);
+    }
+
+}       
+      
+}        
