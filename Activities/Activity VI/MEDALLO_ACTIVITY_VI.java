806f104b24b409af76425c902649af0cd14fde5c
Medallo S. Reynaldo Jr <156997504+medallo272004@users.noreply.github.com>
2024-02-08 16:08:49 +0800

    Add files via upload

---
+++ Activities/Activity VI/MEDALLO_ACTIVITY_VI.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MEDALLO_ACTIVITY_VI {
+ public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println( "Enter the time (is seconds): ");
+    int time = sc.nextInt
+    
+ }  
+}
