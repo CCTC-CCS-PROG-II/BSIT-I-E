176ad68c11ea0a4bc92e87b62f85da32a98c8903
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-02-08 16:08:37 +0800

    Add files via upload

---
+++ Activities/Activity VI/REBIAS_ACTIVITY_6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class REBIAS_ACTIVITY_6 {
+    
+    public static void main(String[] ) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter time (in seconds): ");
+        int time = sc.nextInt();
+        
+
+    }
+}
