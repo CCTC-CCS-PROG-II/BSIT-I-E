446196521194dd3b77673249afae775525e0482a
Dave Diaz <156997121+DhaboyDiaz@users.noreply.github.com>
2024-03-07 16:14:03 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/DIAZ_ACTIVITY_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DIAZ_ACTIVITY_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] scores = new double[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter score " + (i + 1) + ": ");
+            scores[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (double score : scores) {
+            sum += score;
+        }
+
+        double average = sum / scores.length;
+        System.out.println("Average: " + average);
+    }
+}
