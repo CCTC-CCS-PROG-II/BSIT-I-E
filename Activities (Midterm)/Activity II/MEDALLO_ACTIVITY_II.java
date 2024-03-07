9407bcedd53c15804bed4ef1010a542c94ec7146
Medallo S. Reynaldo Jr <156997504+medallo272004@users.noreply.github.com>
2024-03-07 16:14:30 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/MEDALLO_ACTIVITY_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MEDALLO_ACTIVITY_II
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double[] scores = new double[10];
+    
+    System.out.println("Enter 10 scores: ");
+    for (int i = 0; i<10; i++) {
+        scores[i] = sc.nextDouble();
+    }
+    //Calculate average
+    double sum=0;
+    for (double score : scores) {
+        sum += scores;
+    }
+    double average = sum /10;
+
+    //Print results
+    System.out.println("Average score: + average");
+
+    }
+
+    } 
+}
