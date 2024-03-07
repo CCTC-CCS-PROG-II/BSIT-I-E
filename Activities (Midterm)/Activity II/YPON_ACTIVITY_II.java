dba2cf392330afbb96a180ba4d9dd6efc944c78f
Sheenley Von P. Ypon <156996942+Sheenley@users.noreply.github.com>
2024-03-07 16:14:00 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/YPON_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class YPON_ACTIVITY_II{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double [] scores = new double[10];
+
+        for (int i = 0; i < scores.length; i++){
+            System.out.println("Enter score " + (i + 1) + ": ");
+            scores[i] = sc.nextDouble();
+        }
+        double sum = 0;
+        for (double score : scores){
+            sum += score;
+        }
+        double average = sum / scores.length;
+        System.out.println("Average: " + average);
+    }
+    
+}
