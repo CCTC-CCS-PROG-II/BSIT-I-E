8970ae0f5bac83c672c28141b5159ae646a4449d
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-03-07 16:15:24 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/DEGUZMAN.java
@@ -0,0 +1,25 @@
+public class DEGUZMAN {
+    import java.util.Scanner;
+
+        
+                public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+                    double[] scores = new double[10];
+            
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Enter score " + (i + 1) + ": ");
+                        scores[i] = sc.nextDouble();
+                    }
+            
+                  double sum = 0;
+                  for(double score : scores) {
+                    sum += score;
+                  } double average = sum / scores.length;
+                  System.out.println("Average: "+ average);
+            
+                    
+                }
+            }
+            
+
+
