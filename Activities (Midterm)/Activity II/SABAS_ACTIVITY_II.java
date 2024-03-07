3b915f6cedd65507770b8a48b9ab6b0763f833ec
John Kenneth Sabas <156997820+KennethSabas@users.noreply.github.com>
2024-03-07 16:15:28 +0800

    SABAS_ACTIVITY_II.java

---
+++ Activities (Midterm)/Activity II/SABAS_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ALSADO_ACTIVITY_2{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int[]scores = new int[10];
+        int sum = 0;
+        System.out.println("Enter 10 scores: ");
+        for(int i = 0; i < 10; i++) {
+            System.out.print("Enter score " + (i + 1) + ":");
+            scores[i] = sc.nextInt();
+            sum += scores[i];
+        }
+        int average = sum / 10;
+
+        System.out.println("Average score: " + average);
+    }
+}
