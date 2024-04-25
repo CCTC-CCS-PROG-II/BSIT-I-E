1e627b86825f1b75e4545a76118bf4a57c1a31ba
Neco tolero <157685989+Necokyutie@users.noreply.github.com>
2024-04-25 16:08:17 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/TOLERO_ACTIVITY_I.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TOLERO_ACTIVITY_I {
+ static int length(){
+
+    Scanner Kyutie = new Scanner (System.in);
+    System.out.println("Enter the value of length")
+    int length = Kyutie.nextInt();
+    return length;
+    }
+  static int width(){
+    System.out.println("Enter the value of width");
+    return Kyutie.nextInt();
+    }
+    public static void main (String[]args){
+    int result = length() * width()
+    length();
+    width();
+    System.out.println("The area of the rectangle is: " length * width);
+
+    }
+
+
+}
+
+
+
