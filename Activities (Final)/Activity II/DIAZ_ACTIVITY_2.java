9027e750932fd402807d83474e0258523ae3594b
Dave Diaz <156997121+DhaboyDiaz@users.noreply.github.com>
2024-04-25 16:07:01 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/DIAZ_ACTIVITY_2.java
@@ -0,0 +1,13 @@
+import.java.util.Scanner;
+public class DIAZ_ACTIVITY_2 {
+    static void feetToInches= (double feet){
+        double res= feet*12;
+         System.out.println("res");           
+  }
+    public ctatic void main(String[]args){
+          Scanner sc= new Scanner(System,in);
+          System.out.print("Enter a feet:");
+             double feet= sc.nextDouble();
+                 feetToInches(feet);  
+ }
+}
