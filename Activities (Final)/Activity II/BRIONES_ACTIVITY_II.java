fc65b150ab6c4eaba1e5d3da0b38ec120d4df809
Ganny Briones <156997601+gannybear1@users.noreply.github.com>
2024-04-25 16:07:04 +0800

    BRIONES_ACTIVITY_II.java

---
+++ Activities (Final)/Activity II/BRIONES_ACTIVITY_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BRIONES_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of feet:");
+        double feet = scanner.nextDouble();
+
+        double inches = convertFeetToInches(feet);
+
+        System.out.println("Inches: " + inches);
+
+        scanner.close();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12;
+    }
+}
