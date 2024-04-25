03f033e69a48bdc8e62e173cafa714b2889ef440
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-04-25 16:07:07 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/CANTEBEROS_ACTIVITY_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CANTEBEROS_ACTIVITY_II {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter the number of feet:");
+        double feet = scanner.nextDouble();
+
+        
+        double inches = convertFeetToInches(feet);
+
+        
+        System.out.println("Inches: " + inches);
+
+        scanner.close();
+    }
+
+    
+    public static double convertFeetToInches(double feet) {
+        return feet * 12;
+    }
+}
