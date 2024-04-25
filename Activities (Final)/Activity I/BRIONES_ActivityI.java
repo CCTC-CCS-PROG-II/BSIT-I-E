b2a8f8d87f0998ca075deb887a934068efbfa8b6
Ganny Briones <156997601+gannybear1@users.noreply.github.com>
2024-04-25 15:24:00 +0800

    BRIONES_ActivityI.java

---
+++ Activities (Final)/Activity I/BRIONES_ActivityI.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BRIONES_ActivityI {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter the length of the rectangle:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the rectangle:");
+        double width = scanner.nextDouble();
+
+
+        double area = calculateArea(length, width);
+
+      
+        System.out.println("Area: " + area);
+
+        scanner.close();
+    }
+
+   
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+}
