31eb3bcb71ed88da47d59a7ccbb1249f5c81a8d1
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-04-25 16:07:18 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/TOLERO_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+import java_util_Scanner;
+public class TOLERO_ACTIVITY_II{
+    static void feet(double feet){
+        double result = feet * 12;
+        System.out.println("Result: " + result);
+    }
+    public static void main (String []args){
+        Scanner lloyd = new Scanner(System.in);
+        System.out.println("Enter a  value for feet: ");
+        feet(feet);
+    }
+}
