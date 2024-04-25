1631568908b0087749a3db7b945c7f3401cb6f95
Hazzel Consolacion <156997534+hazehazyyyy@users.noreply.github.com>
2024-04-25 16:07:03 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/GAMBOA_ACTIVTY_1.java
@@ -0,0 +1,22 @@
+import java util.Scanner;
+public class GAMBOA_ACTIVTY_1{
+    public static void rectangle(String[]args){
+
+        System.out.println("Enter the length of the rectangular:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the rectangular:");
+        double width = scanner.nextDouble();
+
+        double area = calculateArea(length, width);
+
+        System.out.println("Area: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateArea(double length, double width){
+        return legth * width;
+        
+    }
+}
