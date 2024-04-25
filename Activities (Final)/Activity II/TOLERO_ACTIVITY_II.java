f4b70d91b12ef882c49b1cc968093beb6d906c3a
Neco tolero <157685989+Necokyutie@users.noreply.github.com>
2024-04-25 16:07:54 +0800

    Add files via upload

--- Activities (Final)/Activity II/TOLERO_ACTIVITY_II.java
+++ Activities (Final)/Activity II/TOLERO_ACTIVITY_II.java
@@ -1,12 +1,15 @@
-import java_util_Scanner;
-public class TOLERO_ACTIVITY_II{
-    static void feet(double feet){
+import java.util.Scanner;
+public class TOLERO_ACTIVITY_II {
+    static void double feet(double feet){
+        
         double result = feet * 12;
-        System.out.println("Result: " + result);
+        System.out.println("Result " +result);
     }
-    public static void main (String []args){
-        Scanner lloyd = new Scanner(System.in);
-        System.out.println("Enter a  value for feet: ");
+    public static void main(String []args){
+      Scanner Kyutie = new Scanner(System.in);
+      System.out.println("Enter a value for feet: ");
         feet(feet);
+    
+    
     }
 }
