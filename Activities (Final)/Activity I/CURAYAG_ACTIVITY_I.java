f23d1c066e77311b10e83c9c4413837fb974b8a3
Sonnyboy C. Curayag <156996922+Sonnyboy29@users.noreply.github.com>
2024-04-25 15:23:17 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/CURAYAG_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CURAYAG_ACTIVITY_I {
+  static int length() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the value of Rectangle: ");
+    return sc.nextInt();
+  }
+
+  static int width() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the value of width: ");
+    return sc.nextInt();
+  }
+
+  public static void main(String[] args) {
+    int result = length() * width();
+    System.out.println("Area of the rectangle: " + length() * width());
+  }
+}
