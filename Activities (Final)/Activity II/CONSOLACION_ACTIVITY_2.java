b5b33aab4932fd026255960a0ca63ce436aff6f6
Hazzel Consolacion <156997534+hazehazyyyy@users.noreply.github.com>
2024-04-25 17:23:50 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/CONSOLACION_ACTIVITY_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CONSOLACION_ACTIVITY_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.println("Enter the number of feet:");
+        double feet = scanner.nextDouble();
+
+        
+        double inches = convertFeetToInches(feet);
+
+       
+        System.out.println("Inches: " + inches);
+
+        scanner.close();
+    }
+
+    public static double convertFeetToInches(double feet) {
+        return feet * 12;
+    }
+}
