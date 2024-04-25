1f0a72cfb4f8663d2cc7cda813d0cc5eb5243d70
andrewsungkip <156997562+andrewsungkip@users.noreply.github.com>
2024-04-25 16:08:37 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/SUNGKIP_ACTIVITY_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SUNGKIP_ACTIVITY_1 {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("enter the height of the rectangle: ");
+       double height = scanner.nextDouble();
+
+      System.out.println("enter the width of the rectangle:");
+      double width = scanner.nextDouble();
+
+      double area = calculatearea(length, width);
+
+      System.out.println("Area: "+ area);
+
+      scanner.close();
+
+      public static double caculatearea(double length, double width) {
+           return length * width;
+       }
+     }
+   }
+ 
+    
