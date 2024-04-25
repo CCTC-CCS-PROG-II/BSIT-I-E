73e23ccba158e0c074dcb2aa4a95e4ed3b5112b0
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-04-25 15:23:04 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/REBIAS_ACTIVITY_I.java
@@ -0,0 +1,27 @@
+Import java.util.Scanner;
+
+public class REBIAS_ACTIVITY_I {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    
+        System.out.println("Enter the length of the rectangle:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the rectangle:");
+        double width = scanner.nextDouble();
+
+        double area = calculateArea(length, width);
+
+        
+        System.out.println("Area: " + area);
+
+        scanner.close();
+    }
+
+    
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+}
