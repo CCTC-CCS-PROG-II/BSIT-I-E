81ef86803c4613fda5caff41876ea63ec8338335
AGUANTA LEE <157687146+Aguanta1@users.noreply.github.com>
2024-04-25 16:08:04 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/AGUANTA_ACTIVITY_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AGUANTA_ACTIVITY_II  {
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt for input
+        System.out.println("Enter the number of feet:");
+        double feet = scanner.nextDouble();
+
+        // Convert feet to inches
+        double inches = convertFeetToInches(feet);
+
+        // Print results
+        System.out.println("Inches: " + inches);
+
+        scanner.close();
+    }
+
+    // Method to convert feet to inches
+    public static double convertFeetToInches(double feet) {
+        return feet * 12;
+    }
+}
+
