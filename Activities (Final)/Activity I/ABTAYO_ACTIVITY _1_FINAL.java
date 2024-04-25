11f542aca9e2cdb24701801c9914032ff6373e09
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-04-25 15:24:06 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/ABTAYO_ACTIVITY _1_FINAL.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public public class ABATAYO_ACTIVITY_1_FINAL.java
+{
+    public static int length;
+    Scanner sc = new Scanner(System.in);System.out.println("Enter the length of the rectangle:");
+    double width = sc.nextDouble();
+
+    System.out.println("Enter the width of the rectangle");
+    double width = sc.nextDouble();
+
+    double area = calculatArea(length, width);System.out.printlm("Area: + area");
+
+    Scanner.close();
+
+    public atatic
+    double calculate
+}
