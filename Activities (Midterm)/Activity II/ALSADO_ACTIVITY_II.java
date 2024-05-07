597121a910878ed90b1d5c9fa526be3018930b01
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-05-07 11:18:22 +0800

    Update and rename ALSADO_ACTIVITY_2.java to ALSADO_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/ALSADO_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/ALSADO_ACTIVITY_II.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ALSADO_ACTIVITY_2{
+public class ALSADO_ACTIVITY_II{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int[]scores = new int[10];
