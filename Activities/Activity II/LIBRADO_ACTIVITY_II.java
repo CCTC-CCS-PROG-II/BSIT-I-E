50994f4f3738862965708f7e919e654385307c47
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-07 12:57:33 +0800

    LIBRADO_ACTIVITY_II.java

--- Activities/Activity II/LIBRADO_ACTIVITY_II.java
+++ Activities/Activity II/LIBRADO_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LIBRADO {
+public class LIBRADO_ACTIVITY_II {
     
     public static void main(String[] args){
         Scanner scanner = new Scanner (System.in);
