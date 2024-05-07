6df13e15b83c1d1dd1f55d80175311ef7dac8187
andrewsungkip <156997562+andrewsungkip@users.noreply.github.com>
2024-05-07 12:57:31 +0800

    Update Sungkip_ActivityII.java

--- Activities (Final)/Activity II/Sungkip_ActivityII.java
+++ Activities (Final)/Activity II/Sungkip_ActivityII.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SUNGIKP_ACTIVITYII{
+public class SUNGIKP_ACTIVITY_II{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
