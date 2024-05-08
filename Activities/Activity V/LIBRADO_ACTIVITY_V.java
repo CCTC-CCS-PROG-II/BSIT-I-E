5e87c1e6af41fb768f8d9948780b462cfe8f8b16
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-08 15:05:44 +0800

    Update and rename LIBRADO_ACTIVITY_5.java to LIBRADO_ACTIVITY_V.java

--- Activities/Activity V/LIBRADO_ACTIVITY_V.java
+++ Activities/Activity V/LIBRADO_ACTIVITY_V.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LIBRADO_ACTIVITY_5 {
+public class LIBRADO_ACTIVITY_V {
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
         int age;
