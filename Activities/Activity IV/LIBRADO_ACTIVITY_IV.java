9af842729002fbb533cf1d94899c5ea4e549dc9d
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-08 15:04:55 +0800

    Update and rename LIBRADO_ACTIVITY_4.java to LIBRADO_ACTIVITY_IV.java

--- Activities/Activity IV/LIBRADO_ACTIVITY_IV.java
+++ Activities/Activity IV/LIBRADO_ACTIVITY_IV.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LIBRADO_ACTIVITY_4 {
+public class LIBRADO_ACTIVITY_IV {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
