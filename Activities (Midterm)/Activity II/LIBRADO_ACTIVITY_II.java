95de75529f4198f4a2f49e54b9bbab3eddeeb960
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-08 15:09:16 +0800

    Update and rename LIBRADO_ACTIVITY_2.java to LIBRADO_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/LIBRADO_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/LIBRADO_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LIBRADO_ACTIVITY_2 {
+public class LIBRADO_ACTIVITY_II {
         
                 public static void main(String[] args) {
                     Scanner sc = new Scanner(System.in);
