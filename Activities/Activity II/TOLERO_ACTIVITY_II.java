1a20847f28a03b8fe2b12fecf976d7d641493fc8
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-05-09 15:05:39 +0800

    Add files via upload

---
+++ Activities/Activity II/TOLERO_ACTIVITY_II.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+    public class TOLERO_ACTIVITY_II {
+
+       public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+       System.out.print("Enter first number: ");
+       int fnum = input.nextInt();
+       System.out.print("Enter second number: ");
+       int snum = input.nextInt();
+       int sum = fnum + snum;
+       System.out.println("The sum is " + sum);
+       System.out.println("The sum is " + sum);
+   }
+}
