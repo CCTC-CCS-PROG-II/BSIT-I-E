504228aa1ea5f8eb1f07c82bf6a89db5a596be7a
John Kenneth Sabas <156997820+KennethSabas@users.noreply.github.com>
2024-05-09 15:31:01 +0800

    Update SABAS_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/SABAS_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/SABAS_ACTIVITY_II.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SABAS_ACTIVITY_2{
+public class SABAS_ACTIVITY_II{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int[]scores = new int[10];
