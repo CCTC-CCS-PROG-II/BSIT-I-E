fdade2018e0f72367d4b12a127896bdf942fc1e5
Francis John Panuncillo <156996985+pransthegreat@users.noreply.github.com>
2024-05-09 14:54:20 +0800

    Update PANUNCILLO_ACTIVITY_I.java

--- Activities (Final)/Activity I/PANUNCILLO_ACTIVITY_1.java
+++ Activities (Final)/Activity I/PANUNCILLO_ACTIVITY_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PANUNCILLO_ACTIVITY_1{
+public class PANUNCILLO_ACTIVITY_I{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
