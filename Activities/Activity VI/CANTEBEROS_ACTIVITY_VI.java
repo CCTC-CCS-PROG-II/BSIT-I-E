6b639196e9381c37f622d83f07d26f388b6918c6
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-05-09 15:48:14 +0800

    Update and rename CANTEBEROS_ACTIVITY_6.java to CANTEBEROS_ACTIVITY_VI.java

---
+++ Activities/Activity VI/CANTEBEROS_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CANTEBEROS_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Enter the time in seconds: ");
+     int time = scanner.nextInt();
+     while (time >=0){
+        System.out.println(time);
+         time--;
+     }
+
+     
+    }
+}
