a85dd74507d4d65c6804782bca7081c79d8c9219
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-05-09 15:44:14 +0800

    Update REBIAS_ACTIVITY_IV.java

--- Activities/Activity IV/REBIAS_ACTIVITY_IV.java
+++ Activities/Activity IV/REBIAS_ACTIVITY_IV.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
-public class REBIAS_ACTIVITY_IV {
-    
-    public static void main(String {}args){
-        Scanner input = new Scanner(System.in);
-        int color;
-        System.out.println("Pick a number (1.Red 2.Green 3.Black): ");
-        int num = input.nextInt();
 
-        switch(color) {
-            case 1:
-              System.out.println("Red");
-              break;
-            case 2:
-              System.out.println("Green");
-              break;
-            case 3:
-              System.out.println("Black");  
+public class REBIAS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a color base on the following choices red goes to box 1, green goes to box 2, black goes to box 3 (red, green, black): ");
+        String color = scanner.nextLine();
+
+        switch (color) {
+            case "red":
+                System.out.println("Goes to box 1");
+                break;
+            case "green":
+                System.out.println("Goes to box 2");
+                break;
+            case "black":
+                System.out.println("Goes to box 3");
+                break;
+            default:
+                System.out.println("Invalid color");
         }
     }
 }
