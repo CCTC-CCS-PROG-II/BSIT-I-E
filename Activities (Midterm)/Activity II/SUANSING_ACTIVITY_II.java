6c06b2cd15aa897708e9bfe1f974541b652e6720
suansingchristinegyle <159116665+suansingchristinegyle@users.noreply.github.com>
2024-05-09 15:25:45 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity II/SUANSING_ACTIVITY_II.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SUANSING_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int grades[] ={1,2,3,4,5,6,7,8,9,10};
+        double total = 0.0;
+        int sum =0;
+        for(int A = 0; A < grades.lenght; A++){
+            System.out.print("enter the index" + (A + 1) +":");
+            grades[A] = input.nextInt();
+            total+=grades;
+            sum/=10;
+
+        }
+        System.out.println(sum);
+    }
+}
