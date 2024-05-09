cfc4d6e96fcb98c395745a83cf759ae8d6cae371
Reymark T Prahinog <156998614+KramyerOret@users.noreply.github.com>
2024-05-09 15:05:09 +0800

    Update PRAHINOG_ACTIVITY_1.java

--- Activities (Final)/Activity I/PRAHINOG_ACTIVITY_1.java
+++ Activities (Final)/Activity I/PRAHINOG_ACTIVITY_1.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
 public class PRAHINOG_ACTIVITY_1 {
-    public static double feettoinches(double feetvalue) {
-        double inches = feetvalue * 12;
-        System.out.println(inches);
-        return inches; 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the rectangle:");
+        double width = scanner.nextDouble();
+
+        double area = calculateArea(length, width);
+
+        System.out.println("Area: " + area);
+
+        scanner.close();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double num = sc.nextDouble();
-        feettoinches(num);
+    public static double calculateArea(double length, double width) {
+        return length * width;
     }
 }
