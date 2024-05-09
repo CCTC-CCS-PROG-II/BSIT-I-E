bee918a43130347917efb9818405fbe10317fe50
edmundtrocio <157685632+edmundtrocio@users.noreply.github.com>
2024-05-09 14:58:22 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/TROCIO_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TROCIO_ACTIVITY_II {
+    static void double feet(double feet){
+        
+        double result = feet * 12;
+        System.out.println("Result " +result);
+    }
+    public static void main(String []args){
+      Scanner Kyutie = new Scanner(System.in);
+      System.out.println("Enter a value for feet: ");
+        feet(feet);
+    
+    
+    }
+}
