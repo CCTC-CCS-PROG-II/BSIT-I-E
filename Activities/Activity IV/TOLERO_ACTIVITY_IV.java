d1bc938dff40a56d2c5f25cf51c7fa34c2b23a7f
Neco tolero <157685989+Necokyutie@users.noreply.github.com>
2024-05-09 15:30:56 +0800

    Update and rename TOLERO_ACTIVITY_4.java to TOLERO_ACTIVITY_IV.java

--- Activities/Activity IV/TOLERO_ACTIVITY_IV.java
+++ Activities/Activity IV/TOLERO_ACTIVITY_IV.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TOLERO_ACTIVITY_4 {
+public class TOLERO_ACTIVITY_IV {
     public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);
