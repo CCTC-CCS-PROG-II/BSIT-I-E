14e9d73134f619e8c7fc50a8f611c9b9c1387054
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-05-09 15:43:30 +0800

    Update CANTEBEROS_ACTIVITY_IV.java

--- Activities/Activity IV/CANTEBEROS_ACTIVITY_IV.java
+++ Activities/Activity IV/CANTEBEROS_ACTIVITY_IV.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
-public class CANTEBEROS_ATIVITY_IV {
 
-    public static void main(String []args){
-        Scanner input = new Scanner(System.in);
-        int color;
-        System.out.println("Pick a number (1.Red 2.Green 3.Black): ");
-        int num = input.nextInt();
+public class CANTEBEROS_ACTIVITY_IV {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        switch(color) {
-            case 1:
-             System.out.println("Red");
-             break;
-            case 2:
-             System.out.println("Green");
-             break;
-            case 3:
-             System.out.println("Black");
+        System.out.print("Enter a color base on the following choices red goes to box 1, green goes to box 2, black goes to box 3 (red, green, black): ");
+        String color = scanner.nextLine();
+
+        switch (color) {
+            case "red":
+                System.out.println("Goes to box 1");
+                break;
+            case "green":
+                System.out.println("Goes to box 2");
+                break;
+            case "black":
+                System.out.println("Goes to box 3");
+                break;
+            default:
+                System.out.println("Invalid color");
         }
     }
 }
