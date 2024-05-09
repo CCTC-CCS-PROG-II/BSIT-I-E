be9798eb4c890fe6cc04fb4c6e56e3c9573a0636
Francis John Panuncillo <156996985+pransthegreat@users.noreply.github.com>
2024-05-09 15:11:28 +0800

    Update PANUNCILLO_ACTIVITY_II.java

--- Activities (Final)/Activity II/PANUNCILLO_ACTIVITY_II.java
+++ Activities (Final)/Activity II/PANUNCILLO_ACTIVITY_II.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class ALISOSO_ACTIVITY_II{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of feet:");
+        double feet = sc.nextDouble();
+        double inches = convertFeetToInches(feet);
+        System.out.print("Inches: " + inches);
+        sc.close();
 
+    }
+    public static double convertFeetToInches(double feet) {
+        return feet * 12;
+    }
+}
