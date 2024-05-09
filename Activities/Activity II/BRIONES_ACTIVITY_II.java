c575fbb57b54196064e89e1c18b98f07f6303e6e
Ganny Briones <156997601+gannybear1@users.noreply.github.com>
2024-05-09 14:57:31 +0800

    BRIONES_ACTIVITY_II.java

---
+++ Activities/Activity II/BRIONES_ACTIVITY_II.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class BRIONES_ACTIVITY_II {
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Enter your first Number: ");
+        int fnum = scanner.nextInt();
+        System.out.println("Enter your Second Number: ");
+        int snum = scanner.nextInt();
+        
+        int sum = fnum + snum;
+        System.out.println("sum is = "+sum);
+        int sub = fnum - snum;
+        System.out.println("Sub is = "+sub);
+        int prod = fnum * snum;
+        System.out.println("Prod is = "+prod);
+
+    }
+}
