9199b716c0fc8b42126eb476685b37b2bb35003d
Sonnyboy C. Curayag <156996922+Sonnyboy29@users.noreply.github.com>
2024-05-09 14:59:45 +0800

    Update CURAYAG_ACTIVITY_V.java

--- Activities/Activity V/CURAYAG_ACTIVITY_V.java
+++ Activities/Activity V/CURAYAG_ACTIVITY_V.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class CURAYAG_ACTIVITY_V {
-  public static void main(Stirng []args) {
+  public static void main(String []args) {
     Scanner sc = new Scanner(System.in);
     int age;
     System.out.print("Enter your age: ");
