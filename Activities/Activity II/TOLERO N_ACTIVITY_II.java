deb255dfd3322d0b9b4269c344b5b7d862f45912
Neco tolero <157685989+Necokyutie@users.noreply.github.com>
2024-05-09 15:38:38 +0800

    Update TOLERO N_ACTIVITY_II.java

--- Activities/Activity II/TOLERO N_ACTIVITY_II.java
+++ Activities/Activity II/TOLERO N_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class JAVELLO_ACTIVITY_II {
+public class TOLERO N_ACTIVITY_II {
     public static void main (String []args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter First Number :");
