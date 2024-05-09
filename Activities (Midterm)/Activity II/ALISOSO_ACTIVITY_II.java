8133a45b544d5dc6044f34e92dd6dd8399cc7fc7
Janber Alisoso <156997592+janber0802@users.noreply.github.com>
2024-05-09 15:17:59 +0800

    Update ALISOSO_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/ALISOSO_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/ALISOSO_ACTIVITY_II.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
- public class PLARISAN_ACTIVITY_II{
+ public class ALISOSO_ACTIVITY_II{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
