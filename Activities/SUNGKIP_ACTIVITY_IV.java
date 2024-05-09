f85ed3ee4390e3bb0d265ef184655f8201170e7c
andrewsungkip <156997562+andrewsungkip@users.noreply.github.com>
2024-05-09 14:51:29 +0800

    Update and rename SUNGKIP_ACTIVITY_4.java to SUNGKIP_ACTIVITY_IV.java

--- Activities/SUNGKIP_ACTIVITY_IV.java
+++ Activities/SUNGKIP_ACTIVITY_IV.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class  SUNGKIP_ACTIVITY_4 {
+public class  SUNGKIP_ACTIVITY_IV {
     public static void main(String[] args) {
     Scanner sc = new =Scanner(System.in);
     System.out.print("enter a color: ");
