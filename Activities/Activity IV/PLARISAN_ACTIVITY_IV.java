0dbecc21cbadb77a2809abf2f06ef4f744a4ffe0
Jay P. Plarisan <156997011+BSITjay@users.noreply.github.com>
2024-05-09 14:40:12 +0800

    Update PLARISAN_ACTIVITY_IV.java

--- Activities/Activity IV/PLARISAN_ACTIVITY_IV.java
+++ Activities/Activity IV/PLARISAN_ACTIVITY_IV.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
+
 public class PLARISAN_ACTIVITY_IV {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String color;
-        System.out.println("The robot will catigorize a color ");
+        System.out.println("The robot will categorize a color ");
         System.out.println("Red goes to #1");
         System.out.println("Green goes to #2");
         System.out.println("Black goes to #3");
         System.out.println();
         System.out.println("Enter the color: ");
-        color = sc.nextLine();
-
-        switch(color){
-            case "Red":
-            System.out.println("1");
-            break;
-            case "Green":
-            System.out.println("2");
-            break;
-            case "Black":
-            System.out.println("3");
-            break;
+        color = sc.nextLine().toLowerCase(); 
+        switch (color) {
+            case "red":
+                System.out.println("1");
+                break;
+            case "green":
+                System.out.println("2");
+                break;
+            case "black":
+                System.out.println("3");
+                break;
             default:
-            System.out.println("Error");
+                System.out.println("Error");
         }
     }
 }
