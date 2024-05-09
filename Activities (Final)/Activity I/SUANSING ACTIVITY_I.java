30b8f3a3ce8b78796a3bde33517df6a644344e06
suansingchristinegyle <159116665+suansingchristinegyle@users.noreply.github.com>
2024-05-09 15:17:32 +0800

    Update and rename Suansing_ActivityI.java to SUANSING ACTIVITY_I.java

--- Activities (Final)/Activity I/SUANSING ACTIVITY_I.java
+++ Activities (Final)/Activity I/SUANSING ACTIVITY_I.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Suansing_ActivityI {
+public class SUANSING_ActivityI {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
