a89f39b5d997559e9f8bbc6693ba823a2b9a2681
Reymark T Prahinog <156998614+KramyerOret@users.noreply.github.com>
2024-05-09 16:00:39 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/HERMOSA_ACTIVITY_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HERMOSA_ACTIVITY_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of the rectangle:");
+        double width = scanner.nextDouble();
+
+        double area = calculateArea(length, width);
+
+        System.out.println("Area: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+}
