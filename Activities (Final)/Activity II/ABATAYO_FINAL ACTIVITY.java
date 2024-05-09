b18cf650619af1702dc7fb129b3e7c38c8220211
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-05-09 15:07:26 +0800

    Update ABATAYO_ACTIVITY_II.java

--- Activities (Final)/Activity II/ABATAYO_FINAL ACTIVITY.java
+++ Activities (Final)/Activity II/ABATAYO_FINAL ACTIVITY.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
 public class ABATAYO_ACTIVITY_II {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of feet:");
-        double feet = sc.nextDouble();
-        double inches = sc.nextDouble();
-        System.out.print("Inches:" + inches);
-        sc.close();
+    static void feetToInches(double feet){
+        double result = feet * 12;
+        System.out.printIn("Result: " + result);
     }
+    public static void main(Sring[] args){
+        Scanner sc = new Scanner(System.in);
 
-    public static double convertfeettoInches(double feet) {
-        return feet * 12;
-
+        System.out.printIn("Enter a feet: ");
+        double feet = sc.nextDouble();
+        feetToInches(feet);
     }
 
 }
