d35a1cd0c9331537f8ec8029bdd982591bbfe253
edmundtrocio <157685632+edmundtrocio@users.noreply.github.com>
2024-05-09 14:56:14 +0800

    Update TROCIO_ACTIVITY_2.java

--- Activities (Midterm)/Activity II/TROCIO_ACTIVITY_2.java
+++ Activities (Midterm)/Activity II/TROCIO_ACTIVITY_2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TROCIO_ACTIVITY_2 {
+public class TROCIO_ACTIVITY_II {
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
         int[] scores = new int[10];
