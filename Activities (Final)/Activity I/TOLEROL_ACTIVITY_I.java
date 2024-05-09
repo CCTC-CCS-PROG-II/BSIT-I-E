b96b7ab750d7c9ce443f666ed34d30f8b3443cfa
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-05-09 15:19:24 +0800

    Update and rename TOLERO_ACTIVITY_1.java to TOLEROL_ACTIVITY_I.java

---
+++ Activities (Final)/Activity I/TOLEROL_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TOLEROL_ACTIVITY_I {
+ static int lenght(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the length of the rectangle:");
+    return sc.nextInt();
+}
+static int width(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the width of the rectangle:");
+    return sc.nextInt();
+}
+public static void main(String[]args){
+   int result = lenght()*width();
+ System.out.println("Area of the rectangle;" +lenght()*width());                                                        
+   }
+}
