07fadc8bb769db9bc877f3e47ea2b0eeff35eb3e
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-05-09 15:24:09 +0800

    Update CANTEBEROS_ACTIVITY_I.java

--- Activities (Final)/Activity I/CANTEBEROS_ACTIVITY_I.java
+++ Activities (Final)/Activity I/CANTEBEROS_ACTIVITY_I.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
-
 public class CANTEBEROS_ACTIVITY_I {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-    
-        System.out.println("Enter the length of the rectangle:");
-        double length = scanner.nextDouble();
-
-        System.out.println("Enter the width of the rectangle:");
-        double width = scanner.nextDouble();
-
-        
-        double area = calculateArea(length, width);
-
-        System.out.println("Area: " + area);
-
-        scanner.close();
+    static int length() {
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the rectangle:");
+        return sc.nextInt();
     }
-
-    public static double calculateArea(double length, double width) {
-        return length * width;
+    
+    static int width() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the width of the rectangle:");
+        return sc.nextInt();
+    }
+    public static void main(String[] agrs) {
+        int result = length() * width();
+        System.out.println("Area of the rectangle: " + result); 
     }
 }
