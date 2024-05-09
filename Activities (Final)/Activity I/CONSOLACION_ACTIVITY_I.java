5e9fbef0ae923a7f6d519d4dc7378db87579d3b5
Reymark T Prahinog <156998614+KramyerOret@users.noreply.github.com>
2024-05-09 15:05:35 +0800

    Update CONSOLACION_ACTIVITY_I.java

--- Activities (Final)/Activity I/CONSOLACION_ACTIVITY_I.java
+++ Activities (Final)/Activity I/CONSOLACION_ACTIVITY_I.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class CONSOLACION_ACTIVITY_I {
-
+public class CONSOLACION_ACTIVITY_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
