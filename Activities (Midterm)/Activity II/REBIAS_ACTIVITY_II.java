49c15e4207a4675ff29e866cb337fa2b4003f989
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-05-09 15:21:42 +0800

    Update REBIAS_ACTIVITY_II.java

--- Activities (Midterm)/Activity II/REBIAS_ACTIVITY_II.java
+++ Activities (Midterm)/Activity II/REBIAS_ACTIVITY_II.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-
 public class REBIAS_ACTIVITY_II {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String [] scores = new int[10];
-
-        for (int i = 0; i < scores.lenght; i++) {
-            System.out.print("Enter score " + (i + 1) + ": ");
-            scores[i] = sc.nextLine();
-            sum += i;
-        }
-        System.out.println(sum);
-    }
-}
+        
+                public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+                    double[] scores = new double[10];
+            
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Enter score " + (i + 1) + ": ");
+                        scores[i] = sc.nextDouble();
+                    }
+            
+                  double sum = 0;
+                  for(double score : scores) {
+                    sum += score;
+                  } double average = sum / scores.length;
+                  System.out.println("Average: "+ average);
+            
+                    
+                }
+            }
