c411e2efcf9d86a6e2bdd59ee6ffa6cffee77bde
Dave Diaz <156997121+DhaboyDiaz@users.noreply.github.com>
2024-05-09 14:57:06 +0800

    Update DIAZ_ACTIVITY_IV.java

--- Activities/Activity IV/DIAZ_ACTIVITY_4.java
+++ Activities/Activity IV/DIAZ_ACTIVITY_4.java
@@ -1,5 +1,5 @@
  import java.util.Scanner;
-    public class DIAZ_ACTIVITY_4 {
+    public class DIAZ_ACTIVITY_IV {
         public static void main(String[] args){
             Scanner scanner = new Scanner(System.in);
             System.out.println ("Enter a color");
