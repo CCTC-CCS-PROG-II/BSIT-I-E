e9cbe0646e5d23e2397b1f8627117ab4f43f88c9
Dave Diaz <156997121+DhaboyDiaz@users.noreply.github.com>
2024-05-09 14:54:45 +0800

    Update DIAZ_ACTIVITY_I.java

--- Activities/Activity II/DIAZ_ACTIVITY_1.java
+++ Activities/Activity II/DIAZ_ACTIVITY_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DIAZ_ACTIVITY_1 {
+public class DIAZ_ACTIVITY_I {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
