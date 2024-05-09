68c2bbaae73f7e687427d85c5e11dbff61b85c68
Rieda Mae Ygay <156997539+RiedaYgay@users.noreply.github.com>
2024-05-09 16:06:51 +0800

    Update YGAY_ACTIVITY_VI.java

--- Activities/Activity VI/YGAY_ACTIVITY_VI.java
+++ Activities/Activity VI/YGAY_ACTIVITY_VI.java
@@ -1,8 +1,16 @@
 imoprt java.util.Scanner;
 
 public class YGAY_ACTIVITY_VI {
-Scanner sc = new Scanner (System.in);
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-System.out.prinln("Enter time:");
-int num = sc nextInt();
+     System.out.println("Enter the time in seconds: ");
+     int time = scanner.nextInt();
+     while (time >=0){
+        System.out.println(time);
+         time--;
+     }
+
+     
+    }
 }
