0c86ec2c723582da72759eba89d59c68e2084a11
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-09 15:25:59 +0800

    Add files via upload

---
+++ Activities/Activity II/DEGUZMAN_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class DEGUZMAN_ACTIVITY_II {
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("Enter your first Number: ");
+        int fnum = scanner.nextInt();
+        System.out.println("Enter your Second Number: ");
+        int snum = scanner.nextInt();
+        
+        int sum = fnum + snum;
+        System.out.println("sum is = "+sum);
+        int sub = fnum - snum;
+        System.out.println("Sub is = "+sub);
+        int prod = fnum * snum;
+        System.out.println("Prod is = "+prod);
+
+
+        
+
+    }
+}
