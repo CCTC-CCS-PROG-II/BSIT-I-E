1de5bb0b6eafd0bf342dc41bb5e9cd531afeb1d8
Janber Alisoso <156997592+janber0802@users.noreply.github.com>
2024-05-09 15:08:33 +0800

    Update ALISOSO_ACTIVITY_ IV.java

--- Activities/Activity IV/ALISOSO_ACTIVITY_ IV.java
+++ Activities/Activity IV/ALISOSO_ACTIVITY_ IV.java
@@ -1,22 +1,23 @@
-import java.util.Scanner;
+mport java.util.Scanner;
+
 public class ALISOSO_ACTIVITY_IV {
-    public static void main (String []args){
-        Scanner sc = new Scanner (System.in);
-        System.out.print("Enter the color:");
-        String robot;  sc.nextLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the color: ");
+        String color = sc.nextLine(); // Read color input from the user
         
-        switch (robot){
+        switch (color.toUpperCase()) { // Convert input to uppercase for case-insensitivity
             case "RED":
-            System.out.println("Number 1");
-            break;
+                System.out.println("Number 1");
+                break;
             case "GREEN":
-            System.out.println("Number 2");
-            break;
+                System.out.println("Number 2");
+                break;
             case "BLACK":
-            System.out.println("Number 3");
-            break;
+                System.out.println("Number 3");
+                break;
             default:
-            System.out.println("None");
-        }     
+                System.out.println("None");
+        }
     }
 }
