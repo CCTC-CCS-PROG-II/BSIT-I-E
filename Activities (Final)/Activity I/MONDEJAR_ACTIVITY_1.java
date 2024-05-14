35a00d7d2136e364250f723b3c132e9b9fa89dc3
Lynch Schaeler L. Mondejar <157685644+Lynch38@users.noreply.github.com>
2024-05-14 21:54:19 +0800

    Add files via upload

---
+++ Activities (Final)/Activity I/MONDEJAR_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public public class MONDEJAR_ACTIVITY_I.java {
+    Scanner sc = new Scanner(Scanner.in);
+    public static int length(){
+        System.out.println("Enter the value of lenght");
+        return sc.nextInt();
+   }
+    static int width(){
+        System.out.printIn("Enter the value of width");
+        return sc.nextInt();
+    }
+    public static void main(String []args){
+        int result = lenght(); * Width();
+        System.out.printIn("The area of rectangle is :" + result;
+        
+    }
+}
