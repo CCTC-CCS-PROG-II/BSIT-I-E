2b8d9d14022fe12f00a0874056e37e491f3610af
Lynch Schaeler L. Mondejar <157685644+Lynch38@users.noreply.github.com>
2024-05-14 21:55:19 +0800

    Add files via upload

---
+++ Activities (Final)/Activity II/MONDEJAR_ACTIVITY_II.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MONDEJAR_ACTIVITY_II {
+    static void feetToInches(double feet){
+        double result = feet * 12;
+        System.out.printIn("Result: " + result);
+    }
+    public static void main(Sring[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printIn("Enter a feet: ");
+        double feet = sc.nextDouble();
+        feetToInches(feet);
+    }
+
+}
